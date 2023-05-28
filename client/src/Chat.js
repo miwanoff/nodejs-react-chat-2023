e2cb74c5a7d225d5177a41ab86eb880b55f665e0
miwanoff <marina.iva.noffa@gmail.com>
2023-05-28 13:40:18 +0300

    Finish

--- client/src/Chat.js
+++ client/src/Chat.js
@@ -1,14 +1,20 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useRef } from "react";
 
 const Chat = ({ socket, username, room }) => {
   const [currentMessage, setCurrentMessage] = useState("");
   const [messageList, setMessageList] = useState([]);
+  const effectRan = useRef(false);
 
   useEffect(() => {
-    socket.on("receive_message", (data) => {
-      console.log(data);
-      setMessageList((list) => [...list, data]);
-    });
+    if (effectRan.current === false) {
+      socket.on("receive_message", (data) => {
+        console.log(data);
+        setMessageList((list) => [...list, data]);
+      });
+      return () => {
+        effectRan.current = true;
+      };
+    }
   }, [socket]);
 
   const sendMessage = async () => {
@@ -24,6 +30,7 @@ const Chat = ({ socket, username, room }) => {
       };
 
       await socket.emit("send_message", messageData);
+      setMessageList((list) => [...list, messageData]);
       setCurrentMessage("");
     }
   };
@@ -35,7 +42,23 @@ const Chat = ({ socket, username, room }) => {
       </div>
       <div className="chat-body">
         {messageList.map((messageContent) => {
-          return <h2>{messageContent.message}</h2>;
+          return (
+            <div
+              className="message"
+              id={username === messageContent.author ? "you" : "other"}
+              key={makeid(6)}
+            >
+              <div>
+                <div className="message-content">
+                  <p>{messageContent.message}</p>
+                </div>
+                <div className="message-meta">
+                  <p id="time">{messageContent.time}</p>
+                  <p id="author">{messageContent.author}</p>
+                </div>
+              </div>
+            </div>
+          );
         })}
       </div>
       <div className="chat-footer">
@@ -54,3 +77,14 @@ const Chat = ({ socket, username, room }) => {
 };
 
 export default Chat;
+
+function makeid(length) {
+  var result = "";
+  var characters =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  var charactersLength = characters.length;
+  for (var i = 0; i < length; i++) {
+    result += characters.charAt(Math.floor(Math.random() * charactersLength));
+  }
+  return result;
+}
